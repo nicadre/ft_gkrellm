91163ba6a8fcc23c4bf968e786ce1b4d4bd0387f
Leo LAPILLONNE <llapillo@e1r5p14.42.fr>
2015-04-18 17:40:23 +0200

    todo list and add refresh() to IMonitorModule

--- includes/CPUModule.class.hpp
+++ includes/CPUModule.class.hpp
@@ -12,9 +12,23 @@ private:
 	CPUModule(CPUModule const &);
 
 	CPUModule const			&operator=(CPUModule const &);
+	void					_toto(std::pair<std::string, std::string> & pair);
 
 public:
 	CPUModule(void);
+
+	/*
+	** pour chaque info, il faut faire une fonction en private
+	** qui va return la sortie de getInfos();
+	** refresh(void): for_each(_infos.begin(), _infos.end(), _toto)
+	** _toto(pair) { this->refresh(pair.first()); }
+	**
+	** refresh(string): funptr;
+	**      this->_infos[string] = (this->*(funptr[i].func))();
+	*/
+
+	void			refresh(void);
+	void			refresh(std::string const & what);
 
 	virtual ~CPUModule(void);
 
--- includes/IMonitorModule.interface.hpp
+++ includes/IMonitorModule.interface.hpp
@@ -8,6 +8,8 @@ class IMonitorModule {
 public:
 	virtual ~IMonitorModule(void) {}
 	virtual std::map<std::string, std::string>const	&infos(void) const = 0;
+	virtual void									refresh(void) = 0;
+	virtual void									refresh(std::string const & what) = 0;
 };
 
 std::string	getInfos(std::string const & info);
