ee9b654ea2ddb769e02853b28dff455494970634
Leo LAPILLONNE <llapillo@e1r5p14.42.fr>
2015-04-18 23:19:34 +0200

    add hostname

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,18 +1,24 @@
 #include "CPUModule.class.hpp"
+#include "HostUsernameModule.class.hpp"
 #include <iostream>
 
 int		main(int ac, char **av) {
 	(void)ac;
 	(void)av;
 
-	CPUModule		cpu;
+	CPUModule			cpu;
+	HostUsernameModule	host;
 
 	cpu.refresh();
+	host.refresh();
 
 	IMonitorModule::t_infos		cpuInfos = cpu.infos();
+	IMonitorModule::t_infos		hostInfos = host.infos();
 
 	std::cout << "Welcome\n" << std::endl;
 	std::cout << "~~~~ CPU informations ~~~~\n"
 			  << "Number of CPUs: " << cpuInfos["nbCPU"] << "\n"
 			  << "Type of CPU: " << cpuInfos["typeCPU"] << "\n" << std::endl;
+	std::cout << "~~~~ host / user informations ~~~~\n"
+			  << "Hostname: " << hostInfos["hostName"] << "\n" << std::endl;
 }
